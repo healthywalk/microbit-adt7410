dc586865357944fc5a7209a5090f848725d0814d
healthywalk <89001945+healthywalk@users.noreply.github.com>
2021-08-16 21:56:08 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,12 @@ namespace ADT7410 {
     /**
      * measure Temperature with ADT7410
      */
-    //% blockId=TemperaturewithADT7410
-    //% block="current temperature $i2caddress"
-    export function measureTemperature(i2caddress: number): number {
+    //% blockId=TemperaturewithADT7410I2C
+    //% block="current temperature I2C=$i2caddress"
+    
+    export function measureTemperatureI2C(i2caddress: number): number {
         let rawdata = 0
         let temperature = 0
-        if (i2caddress<=0) i2caddress = 72
         rawdata = pins.i2cReadNumber(i2caddress, NumberFormat.UInt16BE, false)
         rawdata = Math.trunc(rawdata / 8)
         if (4095 < rawdata) {
@@ -21,4 +21,10 @@ namespace ADT7410 {
         temperature = rawdata / 16
         return temperature
     }
+    //% blockId=TemperaturewithADT7410
+    //% block="current temperature"
+    export function measureTemperature(): number {
+        return measureTemperatureI2C(72)
+    }
+
 }
